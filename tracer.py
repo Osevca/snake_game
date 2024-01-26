cf15876adda9dcb6fd6521ccca140181678e562f
Osevca <ondrej.sevca@gmail.com>
2024-01-26 11:39:42 +0100

    snake_game

---
+++ tracer.py
@@ -0,0 +1,14 @@
+from turtle import Turtle, Screen
+import time
+
+screen = Screen()
+
+screen.bgcolor("light green")
+screen.title("Vitejte ve Snake game")
+screen.setup(width= 600, height= 600)
+screen.tracer(1)
+
+
+
+
+screen.exitonclick()
